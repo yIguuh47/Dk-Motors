536c8e6933480aa4350160177af97b81d5a3f62d
Igor Damasceno de Sousa <74266068+yIguuh47@users.noreply.github.com>
2021-11-30 12:27:01 -0300

    commit final
    versão final do projeto!

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length -1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 555}px)`;
+}
+
+setInterval(carrossel, 2600);
+
+
